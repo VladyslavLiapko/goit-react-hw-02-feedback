a485c3258186bfb729988a3372219748a7e00ccb
VladyslavLiapko <109310520+VladyslavLiapko@users.noreply.github.com>
2023-02-26 19:42:11 +0200

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Section from './Section/Section.jsx';
 import Statistics from './Statistics/Statistics.jsx';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Notification from './Notification/Notification';
+
 
 
 const options = ['good', 'bad', 'neutral']
@@ -18,8 +18,6 @@ export default class App extends Component {
     };
   
   countTotalFeedback = () => {
-      console.log("Inside cout feedback")
-    console.log(this.state)
       const { good, neutral, bad } = this.state;
       return good + bad + neutral;
     };
@@ -31,20 +29,8 @@ export default class App extends Component {
     };
   
   handleFeedback = ({ target }) => {
-    console.log(target)
-    console.log(target.dataset)
-
-    if (target.dataset.feedback === 'bad') {
-      this.state.bad += 1
-    } else if (target.dataset.feedback === 'good') {
-      this.state.good += 1
-    } else {
-      this.state.neutral += 1
-    }
-    const { Feedback } = this.state;
-    this.setState(prevState => {
-      return { [Feedback]: prevState[Feedback] + 1 };
-    });
+    const feedback = target.dataset.feedback
+    this.setState({[feedback]: this.state[feedback] + 1})
   };
   
     render() {
